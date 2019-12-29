74b2cf323baf4f2b9ff54e1a667e922137751c7a
Christine Cua <cloudycua@gmail.com>
2019-12-29 14:42:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-// Code your solution here!
+function printString(string) {
+  console.log(string[0]);
+  if (string.length > 1) {
+    printString(string.substring(1, string.length))
+  } else {
+    return true
+  }
+}
 
+function reverseString(string) {
+  if (string.length < 2) {
+    return string;
+  } else {
+    return reverseString(string.substring(1)) + string[0];
+  }
+}
+
+function isPalindrome(string) {
+  if (string.length === 1) {
+    return true
+  }
+  if (string.length > 1) {
+    if (string[0] === string[string.length - 1]) {
+      isPalindrome(string.slice(1, string-length - 1))
+    } else {
+    return false
+    }
+  } else {
+    return false
+  }
+}
